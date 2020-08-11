fc554b2f0f6f14ae7a3b47239234887b9f0d6f9d
Dpkell <donald.kell92@gmail.com>
2020-08-11 09:14:56 -0400

    Added notes to Sign-Up component

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -10,6 +10,9 @@ import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 import './sign-up.styles.scss';
 
 const SignUp = ({history}) => {
+
+    // User Credentials State
+
     const [userCredentials, setUserCredentials] = useState({
         displayName: '',
         email: '',
@@ -17,10 +20,18 @@ const SignUp = ({history}) => {
         confirmPassword: ''
     });
 
+    // Loading State
+
     const [isLoading, setIsLoading] = useState(false);
 
-    
+    /**
+     * useRef is used to becasue we a reference object pointed to the component for the full lifetime of the component.
+     * This is especially true since we are using async functions within the component and need to make sure that the 
+     * component properly unmounts using useEffect after our async calls to firebase successfully executes.
+     */
     const _isMounted = useRef(true);
+
+    // Component clean-up/unsubscribe function. Sets .current property of our useRef object to false.
 
     useEffect(() => {
         return () => {
@@ -28,10 +39,23 @@ const SignUp = ({history}) => {
         }
     }, []);
 
+    // Deconstruction of User Credentials to make available for handleChange and handleSubmit functions
+
     const { displayName, email, password, confirmPassword } = userCredentials;
     
+    /**
+     * Handles form submission. Ensures passwords are equal and sets loading state to true. Performs
+     * the firebase user creation with email and password as well as creates the new user document within
+     * firestore. After successfully completing, resets User Credentials state, and resets isLoading state.
+     */
     const handleSubmit = async event => {
         event.preventDefault();
+
+        if (password !== confirmPassword) {
+            alert('Passwords do not match');
+            return;
+        }
+
         setIsLoading(true);
         try {
             if (_isMounted.current) {
